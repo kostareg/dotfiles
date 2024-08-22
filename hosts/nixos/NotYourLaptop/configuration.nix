b131c155e21a499446b208a30f4bcde692d523bd
kostareg <areg.kostanyan@gmail.com>
2024-08-22 09:09:09 -0400

    feat: tailscale

--- hosts/nixos/NotYourLaptop/configuration.nix
+++ hosts/nixos/NotYourLaptop/configuration.nix
@@ -253,6 +253,9 @@
     }
   ];
 
+  services.tailscale.enable = true;
+  services.tailscale.useRoutingFeatures = "both";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
